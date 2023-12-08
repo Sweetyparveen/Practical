5a1da613a5e2e87a9284c1cb972d7c9987f1720c
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-08 16:36:46 +0530

    Add files via upload

---
+++ practical1.js
@@ -0,0 +1,55 @@
+//Program to print text
+// function welcomeMsg(name) {
+	// return ("Hello " + name + " welcome to Accenture");
+// }
+// creating a variable
+// let nameValue = "Sweety";
+
+// calling the function
+// console.log(welcomeMsg(nameValue));
+
+
+
+
+// The prompt() method displays a dialog box that prompts the user for input.
+// program to print the text
+// function myfunction(name) {
+    // console.log("Hello " + name + ":)");
+// }
+// variable name can be different
+// let name = prompt("Enter a name: ");
+// calling function
+// myfunction(name);
+
+
+
+
+//Program to print text
+// function myFunction() {
+//     console.log("Hello World!");
+//   }  
+// myFunction();
+
+
+
+
+
+// function sumOfThreeNumbers(num1, num2, num3) {
+//     console.log(num1 + num2 + num3);
+// }
+// sumOfThreeNumbers(4,5,6)
+// var result = sumOfThreeNumbers(2, 5, 8);
+// console.log(result);
+
+
+
+//Program to access object properties using bracket Notation
+// const person = { 
+//     name: 'John', 
+//     age: 20, 
+// };
+// // accessing property
+// console.log(person["name"]); 
+
+
+
---
+++ practical2.js
@@ -0,0 +1,52 @@
+// let name1="sweety"
+// console.log(name1);
+// let name2="parveen"
+// console.log(name2);
+
+
+// function test(name1,name2){
+//     return name1, name2
+// }
+// console.log("sweety");
+// console.log("Parveen");
+
+
+//program to divide first element by second
+// function creatingfunc(firstvalue, secondvalue) {
+//     return firstvalue / secondvalue;
+// }
+// const value = creatingfunc(8, 2); // Calling the function
+// console.log(value);
+
+
+// program to find the square of a number
+// function is declared inside the variable
+// let x = function (num) { return num * num };
+// console.log(x(4));
+
+// can be used as variable value for other variables
+// let y = x(3);
+// console.log(y);
+
+
+// object
+// const student = {
+//     firstName: 'ram',
+//     class: 10
+// };\
+
+
+//program to access property name
+// const person = { 
+//     name: 'John', 
+//     age: 20, 
+// };
+// console.log(person.name); // John
+
+
+//arrow function
+// number = (a, b) => {
+// 	c = 5;
+// 	return (a + b) * c;
+// };
+// console.log(number(2, 3));
