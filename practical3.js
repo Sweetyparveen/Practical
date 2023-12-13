54314dd8589bd53a905c15cb5734e47facec89cd
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-13 18:43:13 +0530

    Add files via upload

---
+++ practical3.js
@@ -0,0 +1,63 @@
+
+//accessing word in string
+// const word="Sweety"
+// console.log(word[1]);
+
+//use of length
+// const word="abcdefghijklmnopqrsruvx"
+// console.log(word.length);
+
+
+//program to create a new string using new string method
+// let str1 = "Welcome to JavaScript string method";
+// let str2 = new String("Thank you");
+// console.log(str1);
+// console.log(str2);
+
+
+
+//Program to find length of a string
+// function func_to_count_length() {
+// 	console.log("Thermofisher".length)
+// }
+// func_to_count_length();
+
+
+//Program to replace
+// let str = "JavaScript, Html, Python";
+// let part = str.replace("Html", "Reactjs");
+// console.log(part);
+
+
+//program to replace all those words which are same 
+// let str = "Python, JavaScript, Python, Nodejs";
+// let part = str.replaceAll("Python", "CSS");
+// console.log(part);
+
+
+
+//program to do all string in capital letter 
+// let value = 'Welcome to Visual Studio';
+// console.log(value.toUpperCase(value)) ;
+
+
+
+//program to do all string in small letter/lowercase
+// let value = 'Welcome to Visual Studio';
+// console.log(value.toLowerCase(value)) ;
+
+
+//program to concat two string into one using concat method
+// let value1 = 'WHO= ';
+// let value2 = 'stands for World Health Organization';
+// console.log(value1.concat(value2));
+
+
+
+//program to split the string into an array of sub-strings
+// let value = 'Scottish/High/International/school'
+// Split string on '/'. 
+// console.log(value.split('/'))
+
+
+
