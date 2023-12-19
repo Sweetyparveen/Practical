16e75f66bef5f7a05c5cdc5ea52ac19a90181cfe
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-19 11:13:40 +0530

    Add files via upload

---
+++ practical4.js
@@ -0,0 +1,41 @@
+//program to search Private word using search method
+
+// let value= "Accenture Solutions Private Limited"; 
+// let value_result = value.search("Private"); 
+// console.log(value_result);
+
+
+
+//match is an inbuilt function that is used to search a string against regular expression
+// let value= "Accenture Solutions Private Limited";
+// let value_result = value.match(/Private/g); 
+// console.log(value_result);
+
+
+//the method includes checks whether the specific string is there in the code  or not if yes then true else false
+// let value= "Accenture Solutions Private Limited";
+// let value_result = value.includes("Solutions"); 
+// console.log(value_result);
+
+
+//the startsWith method checks if a string is specified in a substring or not
+// let value= "Accenture Solutions Private Limited";
+// let value_result = value.startsWith("Is"); 
+// console.log(value_result);
+
+
+
+//the endsWith() method is used to find the last word from our given string
+// let value= "Accenture Solutions Private Limited"; 
+// let value_result = value.endsWith("Limited"); 
+// console.log(value_result);
+
+
+
+//The lastIndexOf() method is used to find the index of a given string.
+// let value= "Accenture Solutions Private Limited"; 
+// let value_result = value.lastIndexOf("Solutions"); 
+// console.log(value_result);
+
+
+
