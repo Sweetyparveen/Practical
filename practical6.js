d771862621bdf7229d4c5c9be3fc0062feee2645
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-20 17:33:37 +0530

    Add files via upload

---
+++ practical6.js
@@ -0,0 +1,70 @@
+//Program to reverse an array with the help of array method 
+// let numbers_array = [1, 2, 3, 4, 5];
+// console.log("Original Array: ", numbers_array);
+// numbers_array.reverse();
+// console.log("Reversed Array: ", numbers_array);
+
+
+
+//Program to check whether the number is present in the array or not
+// const arr = [21, 12, 13, 45, 54];
+// if (arr.includes(45)) {
+// 	console.log("Number is present in the array.");
+// } else {
+// 	console.log("Number is not present in the array.");
+// }
+
+
+
+
+//Program to remove the last element
+// let myarray=[34, 24, 31, 48];
+// console.log("Array = ", myarray);
+// function myfunction(){
+//     myarray.pop();
+//     console.log("Remaining Array : ", myarray);
+// }
+// myfunction()
+
+
+
+// append array
+// let array1 = ["Accenture1","Accenture2"];
+// let array2= ["Accenture3", "Accenture4"];
+// let array3 = ["Accenture5"];
+
+// let array4 = array1.concat(array2, array3);
+
+// console.log(array4);
+
+
+
+//To convert string to an array
+// let value= "Accenture";
+// // Use String split() method to Convert String to an Array
+// let splited_value = value.split('');
+
+// console.log(splited_value);
+
+
+
+//Program to add an object with the help of push method
+// function pushFunction() {
+// 	list.push("Four", "Five");
+// 	console.log(list);
+// }
+//     let list = ["One", "Two", "Three"];
+// pushFunction();
+
+
+
+
+//Program to sort an array in descending order 
+// let marks = [12, 25, 31, 23, 75, 81, 100];
+
+// console.log("Original Array: "+ marks);
+// marks.sort();
+// console.log("After Sorting in Ascending Order: "+ marks);
+
+
+
