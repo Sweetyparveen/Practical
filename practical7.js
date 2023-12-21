1e3780d271284d7085c30ebad64de3bd48ef6739
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-21 19:30:09 +0530

    Add files via upload

---
+++ practical7.js
@@ -0,0 +1,123 @@
+//check if a number is odd or even
+// function Checkevenodd(number){
+//     if (number%2==0){
+//         console.log("It is a even number");
+//     }
+//     else{
+//         console.log("It is a odd number");
+//     }
+// }
+// Checkevenodd(5)
+
+
+
+//check if the input is number or not
+// function checker(NumverOrNot){
+//     if (isNaN(NumverOrNot)){
+//         console.log("It is not a number");
+//     }
+//     else{
+//         console.log("It is a number");
+//     }
+// }
+// checker("xya")
+// checker(12)
+
+
+
+
+//find the largest of two number
+// function FindLargestNumber(Num1,Num2){
+//     if (Num1>Num2){
+//         console.log(Num1,"is the largest Number");
+//     }
+//     else if(Num2>Num1){
+//         console.log(Num2,"is the largest number");
+//     }
+//     else{
+//         console.log(Num1,"is equal to",Num2);
+//     }
+// }
+// FindLargestNumber(23,3)
+
+
+
+//find the largest of three numbers
+// function largestfunction(num1,num2,num3){
+//     if(num1>num2&&num1>num3){
+//         console.log(num1,"is the largest number");
+//     }
+//     else if(num2>num3){
+//         console.log(num2,"is the largest number");
+//     }
+//     else{
+//         console.log(num3,"is the largest number");
+//     }
+// }
+// largestfunction(21,12,45)
+
+
+
+//program to do calculation
+// function calculator(num1,num2,operator){
+//     if(operator=="add"){
+//         console.log("Sum of",num1,"and",num2,"is:",(num1+num2));
+//     }
+//     else if(operator=="Sub"){
+//         console.log("Difference of",num1,"and",num2,"is:",(num1-num2));
+//     }
+//     else if(operator=="Multiply"){
+//         console.log("Multiplication of",num1,"and",num2,"is:",(num1*num2));
+//     }
+//     else if(operator=="Divide"){
+//         console.log("Division of",num1,"and",num2,"is:",(num1/num2));
+//     }
+//     else{
+//         console.log(operator,"Invalid Operator performed");
+//     }
+// }
+// calculator(45,5,"add")
+// calculator(50,20,"Sub")
+// calculator(4,5,"Multiply")
+// calculator(20,5,"Divide")
+
+
+
+//check is a number is negative or positive
+// function NumberChecker(Number){
+//     if(Number>0){
+//         console.log("The number is positive");
+//     }
+//     else{
+//         console.log("The number is negative");
+//     }
+// }
+// NumberChecker(2)
+
+
+//Program to check divisibility of a number
+// function divisibilitycheck(Number){
+//     if((Number%5==0 )&& (Number%11==0)){
+//         console.log("The number is divisible by 5 and 11");
+//     }
+//     else{
+//         console.log("The number is not divisible by 5 and 11");
+//     }
+// }
+// divisibilitycheck(55)
+
+
+//Say hello if the number is divisible by 5 and if not say bye
+// function HelloBye(){
+//     const number=9
+//     if ((number%5==0)){
+//         console.log("Say Hello");
+//     }
+//     else{
+//         console.log("Say Bye");
+//     }
+// }
+// HelloBye()
+
+
+
