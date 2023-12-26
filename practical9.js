64ca915f5438059dc1869fccfcc55934f2304379
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-12-26 19:17:48 +0530

    Add files via upload

---
+++ practical9.js
@@ -0,0 +1,106 @@
+//Simple while loop that will print numbers from 1-5
+// let i = 1
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+//infinite loop
+// Caution: Infinite Loop
+// while (true) {
+//     console.log("This loop will run forever!");
+//   }
+  
+
+
+//calculating factorial
+// let number = 5;
+// let factorial = 1;
+// let i = 1;
+
+// while (i <= number) {
+//   factorial *= i;
+//   i++;
+// }
+
+// console.log("Factorial of",number ,"is",factorial);
+
+
+
+
+//generating random numbers
+// let randomNumber;
+// while (randomNumber !== 9) {
+//   randomNumber = Math.floor(Math.random() * 10) + 1;
+//   console.log("Random number:" ,randomNumber);
+// }
+
+
+
+//While loop that counts down from 10 to 1,
+// let count = 10;
+
+// while (count >= 1) {
+//   console.log(count);
+//   count--;
+// }
+
+
+
+//While loop that prints all even numbers from 2 to 20
+// let number = 2;
+
+// while (number <= 20) {
+//   console.log(number);
+//   number += 2;
+// }
+
+
+
+//Calculate and print the sum of the squares of numbers from 1 to 5 using a while loop
+// let num = 1;
+// let sumOfSquares = 0;
+// while (num <= 5) {
+//   sumOfSquares += num * num;
+//   num++;
+// }
+// console.log("Sum of squares:",sumOfSquares);
+
+
+
+//Write a while loop to reverse a given string.
+// let inputString = "Hello, World!";
+// let reversedString = "";
+// let index = inputString.length - 1;
+
+// while (index >= 0) {
+//   reversedString += inputString[index];
+//   index--;
+// }
+
+// console.log("Original String:", inputString);
+// console.log("Reversed String:" ,reversedString);
+
+
+
+//Multiplication table of a given number using a while loop
+// let multiplier = 5; //We can change this to any number for a different multiplication table
+// let i = 1;
+
+// console.log("Multiplication Table for", multiplier, "is given below: ");
+
+// while (i <= 10) {
+//   let result = multiplier * i;
+//   console.log(multiplier, "x" ,i, "=",result);
+//   i++;
+// }
+
+
+//User Input
+// let userInput;
+// const prompt=require("prompt-sync")({sigint:true}); 
+// while (userInput !== "exit") {
+//   userInput = prompt("Enter a value (type 'exit' to end):");
+//   console.log(`You entered: ${userInput}`);
+// }
